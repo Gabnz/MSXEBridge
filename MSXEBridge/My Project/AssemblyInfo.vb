d6323801e4340f2836c089b1875287928ee59bc3
Gabriel Nunez <gabriel.nzn@gmail.com>
2015-07-08 07:41:40 -0430

    Primer commit

---
+++ MSXEBridge/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("MSXEBridge")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("MSXEBridge")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(True)> 
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("d7da2db2-8ad4-42c3-87fc-1701cf2f8a7d")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ MSXEBridge/bridge.vb
@@ -0,0 +1,102 @@
+﻿
+Public Class Bridge
+
+    Dim sensor As MiniScanXE.MSXE
+    Const PUNTOS_ESPECTRALES As Integer = 31
+    Public Sub New()
+        sensor = New MiniScanXE.MSXE
+    End Sub
+    Public Function prueba() As String
+
+        Return "Esta es una prueba de comunicacion."
+    End Function
+
+    Public Function Beep() As Boolean
+
+        Dim respuesta As Boolean = True
+
+        Try
+            sensor.Beep()
+        Catch ex As Exception
+            respuesta = False
+        End Try
+
+        Return respuesta
+    End Function
+
+    Public Function abrirPuerto() As Boolean
+
+        Dim i As Integer = 1
+        Dim conectado As Boolean = False
+        sensor.BaudRate = 9600
+        'prueba con cada uno de los puertos COM para conectar el miniscan
+        While i < 5 And conectado = False
+            sensor.CommPort = i
+            Try
+                sensor.OpenCommPort()
+                conectado = True
+            Catch ex As Exception
+                i += 1
+            End Try
+        End While
+
+        Return conectado
+    End Function
+
+    Public Function cerrarPuerto() As Boolean
+
+        Dim desconectado As Boolean = True
+
+        Try
+            sensor.CloseCommPort()
+        Catch ex As Exception
+            desconectado = False
+        End Try
+
+        Return desconectado
+    End Function
+
+    Public Function leerBlanco() As Boolean
+
+        Dim leido As Boolean = True
+
+        Try
+            sensor.ReadWhite()
+        Catch ex As Exception
+            leido = False
+        End Try
+
+        Return leido
+    End Function
+
+    Public Function leerNegro() As Boolean
+
+        Dim leido As Boolean = True
+
+        Try
+            sensor.ReadBlack()
+        Catch ex As Exception
+            leido = False
+        End Try
+
+        Return leido
+    End Function
+
+    Public Function medirMuestra()
+        Dim resultado(PUNTOS_ESPECTRALES) As Single
+
+        Try
+            sensor.ReadSample(resultado)
+        Catch ex As Exception
+
+        End Try
+
+        Return resultado
+    End Function
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+End Class
+
+
